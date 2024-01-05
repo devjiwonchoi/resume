4904216e68e138d29ecee996114c3e4a0c8a50b0
devjiwonchoi <devjiwonchoi@gmail.com>
2024-01-05 22:01:09 +0900

    clear out previous project

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.get('/api', async (req, res) => {
-  res.send('Hello World!')
-})
-
-export default app
--- src/dev-server.ts
+++
@@ -1,5 +0,0 @@
-import app from '../api'
-
-app.listen(8000, () => {
-  console.log('http://localhost:8000')
-})
